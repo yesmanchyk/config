9c441492c15f5f4b42f252cc506d68461d9d2be6
Alex Yesmanchyk <91508478+yesmanchyk@users.noreply.github.com>
2022-06-16 21:20:22 -0400

    Neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,46 @@
+require'plugins' -- Enables :PackerSync
+
+vim.cmd([[
+set expandtab
+set tabstop=4
+set shiftwidth=4
+set number
+startinsert
+]])
+
+local cmp = require'cmp'
+cmp.setup {
+  snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+  },
+  mapping = cmp.mapping.preset.insert({
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'buffer' }
+  }
+}
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- The following example advertise capabilities to `clangd`.
+require'lspconfig'.clangd.setup {
+  capabilities = capabilities,
+}
+
+require'lspconfig'.pyright.setup{
+  capabilities = capabilities
+}
+
